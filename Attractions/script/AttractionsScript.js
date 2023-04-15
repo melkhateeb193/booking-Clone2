1286dd5d1622a7f1f7034a7675a84eb1236cbfe0
kareemawad123 <kareemawad120@gmail.com>
2023-04-15 20:29:03 +0200

    currency v1.0

--- Attractions/script/AttractionsScript.js
+++ Attractions/script/AttractionsScript.js
@@ -10,3 +10,25 @@ function DisplayFunction() {
     }
 }
 
+function SetCurrentStyle(element){
+    element.style.backgroundColor = "#F0F6FD";
+    element.childNodes.forEach(el => {
+        console.log(el);
+        el.style.color = "blue";
+    });
+}
+
+function ChangeCurrency(element){
+    console.log(element.lastChild);
+    var currency = element.lastChild.innerHTML;
+    console.log(currency);
+    document.getElementById("currncy1").innerHTML = currency;
+    SetCurrentStyle(element);
+
+    
+}
+
+function prevStyle(){
+    var elements = document
+}
+
